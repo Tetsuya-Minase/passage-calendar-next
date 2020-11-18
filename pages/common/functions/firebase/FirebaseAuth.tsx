a95bee9bd200259b18b63b44f4b563c758b91df0
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-11-18 14:08:21 +0900

    feet: add firebase components.🔥

---
+++ pages/common/functions/firebase/FirebaseAuth.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+
+import { firebase, FirebaseContext } from './Firebase';
+
+const useFirebaseAuth = () => {
+  const [initialized, setInitialized] = useState(false);
+  const [userId, setUserId] = useState<string | null>(null);
+  const [userName, setUserName] = useState('');
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(async user => {
+      setInitialized(true);
+      setUserId(user ? user.uid : null);
+      setUserName(user ? user.displayName || '' : '');
+    });
+  }, []);
+
+  return { initialized, userId, userName };
+};
+
+type FirebaseAuthProps = {
+  NotSignedIn: React.FC;
+  Loading: React.FC;
+}
+
+export const FirebaseAuth: React.FC<FirebaseAuthProps> = (
+  {
+    children,
+    NotSignedIn ,
+    Loading
+  }
+): JSX.Element => {
+  const { initialized, userId, userName } = useFirebaseAuth();
+  if (!initialized) {
+    return <Loading/>;
+  } else if (!userId) {
+    return <NotSignedIn/>;
+  } else {
+    return (
+      <FirebaseContext.Provider
+        value={{ userId, userName }}
+        children={children}
+      />
+  );
+  }
+};
+
+export const signInWithRedirect = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  return firebase.auth().signInWithRedirect(provider);
+};
+
+export const signOut = () => {
+  return firebase.auth().signOut();
+};
